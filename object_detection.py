03c16e6a9f56b41785bd49043302af380c5e3075
coolashishpt <prasadashish24@gmail.com>
2020-06-30 23:48:58 +0530

    add code for object detection

--- object_detection.py
+++ object_detection.py
@@ -1,3 +1,17 @@
 from model import *
 
 
+def predict(image_path):
+    # just give image path 
+    img = Image.open(image_path)
+#     print(img.size)
+    image = letterbox_image(img, (416, 416))
+#     print(image.size)
+    
+    box, score, classes = detect_objects(image)
+    
+    return show_objects(image, box, score, classes)
+
+
+# give image path or give name with extension if the image is present in the project directory
+predict(r"F:\Computer Vision\Object Detection Yolov3\test.jpg")
